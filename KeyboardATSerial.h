360976b3eb5361cd4d8eb5f20bc11eea56b677b3
David Konsumer <konsumer@jetboystudio.com>
2024-09-18 00:15:51 -0700

    intial commit

---
+++ KeyboardATSerial.h
@@ -0,0 +1,26 @@
+// this just prints to serial when keys are pressed
+
+#include "KeyboardAT.h"
+
+class KeyboardATSerial : public KeyboardAT {
+public:
+  KeyboardATSerial(int clock, int data)
+    : KeyboardAT(clock, data) {
+    Serial.begin(9600);
+  }
+
+  virtual void _update() {}
+
+  virtual void onKeyDown(unsigned char key) {
+    // if (key >= 0x80){  // If it is a modifier key
+    // }
+    Serial.print("UP: ");
+    Serial.println(key);
+  }
+  virtual void onKeyUp(unsigned char key) {
+    // if (key >= 0x80){  // If it is a modifier key
+    // }
+    Serial.print("DOWN: ");
+    Serial.println(key);
+  }
+};
---
+++ keyboard_at.ino
@@ -0,0 +1,19 @@
+#include "KeyboardATSerial.h"
+
+// IBM keyboard is on pins 5 & 6 (clock, data)
+#define PIN_CLOCK 5
+#define PIN_DATA 6
+
+KeyboardATSerial* keeb;
+
+// I do this in an interupt on clock-pin
+void IRAM_ATTR clk_down() {
+  keeb->update();
+}
+
+void setup() {
+  keeb = new KeyboardATSerial(PIN_CLOCK, PIN_DATA);
+  attachInterrupt(digitalPinToInterrupt(PIN_CLOCK), clk_down, FALLING);
+}
+
+void loop() {}
